80e1e9bcd62e498f7352751b2b13f0502521cb80
Dynesshely <catrol@qq.com>
2025-04-23 12:41:51 +0800

    Added windows input method enhancer util project, initialized

---
+++ Workaround.Utils/Enhancer.Windows.InputMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using Workaround.Contract;
+using Workaround.Shared;
+
+namespace Enhancer.Windows.InputMethod;
+
+public class Program : IIdentifier
+{
+    public UtilInfo GetInfo() =>
+        new()
+        {
+            Name = "Windows Input Method Enhancer",
+            Description = "Enhance your windows input method experience.",
+            Version = "0.0.1",
+            Authors = "Dynesshely",
+            UtilActions =
+            [
+                new UtilAction()
+                {
+                    Name = "Simplified Chinese Input Mode Guard",
+                    Description = "To fix the issue that the input method can not stay on default input mode.",
+                    CanExecute = true,
+                    IsNeedToRestart = true,
+                    Execution = () => { },
+                    Reopener = () => { },
+                    EnabledChecker = () => false,
+                },
+            ],
+        };
+}
